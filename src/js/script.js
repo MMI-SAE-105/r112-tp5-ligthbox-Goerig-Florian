226162f668cf16e1601be27c005df431c3ac99e5
Goerig-Florian <florian.goerig@edu.univ-fcomte.fr>
2024-12-04 15:45:09 +0100

    code css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // *** LIGHTBOX ***
+
+const LightboxImg = document.queryselector('#lightbox img');
+const lightbox = document.queryselector('#lightbox');
+document.querySelectorAll('data-full-img]').forEach((imgThumb)=>{
+    imgThumb.addEventListener('click',(evt)=> {
+        LightboxImg.src = imgThumb.dataset.fullImg;
+        lightbox.schowModal()   });
+});
+
+lightbox.addEventListener('click', () => lightbox.close());
